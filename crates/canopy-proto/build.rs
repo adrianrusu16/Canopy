e89d25537e028e4538a3cdbc66231422db69a903
adrianrusu16 <adrianrusu016@gmail.com>
2026-06-23 22:04:35 +0300

    Make proto build robust across path styles

--- crates/canopy-proto/build.rs
+++ crates/canopy-proto/build.rs
@@ -1,8 +1,18 @@
+use std::path::PathBuf;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("cargo:rerun-if-changed=proto/canopy.proto");
+    let manifest_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR")?);
+    let source_proto = manifest_dir.join("proto").join("canopy.proto");
+
+    let out_proto_dir = PathBuf::from(std::env::var("OUT_DIR")?).join("proto");
+    std::fs::create_dir_all(&out_proto_dir)?;
+    let out_proto = out_proto_dir.join("canopy.proto");
+    std::fs::copy(&source_proto, &out_proto)?;
+
+    println!("cargo:rerun-if-changed={}", source_proto.display());
     tonic_prost_build::configure()
         .build_server(true)
         .build_client(true)
-        .compile_protos(&["proto/canopy.proto"], &["proto"])?;
+        .compile_protos(&[out_proto], &[out_proto_dir])?;
     Ok(())
 }
